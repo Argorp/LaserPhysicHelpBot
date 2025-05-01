1486b910a56d51d8adfc6aa49093927ed41a0931
Argorp <virus4buk@yandex.ru>
2025-05-01 20:21:47 +0800

    added upload to vk group

--- Calc_classes/Vk_album.py
+++ Calc_classes/Vk_album.py
@@ -2,60 +2,38 @@ import os
 import vk_api
 import logging
 
-
-LOGIN = ''
-PASSWORD = ''
-ALBUM_ID = 1
-GROUP_ID = 2
+ALBUM_ID = 307346501
+GROUP_ID = 230183619
 
 logging.basicConfig(
     filename='bot_work.log',
     format='%(asctime)s %(levelname)s %(name)s %(message)s'
 )
 
-
 class Vk_upload:
     def __init__(self):
         self.upload = None
-        self.login = LOGIN
-        self.password = PASSWORD
         self.path = os.path.join('static', 'img')
-        self.vk_sess = vk_api.VkApi(
-            login=LOGIN,
-            password=PASSWORD,
-            auth_handler=self.two_factor_handler,
-            captcha_handler=self.captcha_handler
-        )
+        self.vk_sess = vk_api.VkApi(token="vk1.a.pKAkF-xVRr-izepGZBU7PhY2SzOrMsYJjBKPPkwrIfhFYFgwBGFaWDZ0S9D2-slpXJnHUoAguHwV3chzar3i7Rljzs8LpZT4dm9HEBs3S9RnmAfoGpiUzbKXwI2WYJX9RWX86wRrxjfQXdm4rnB_wLwVVRlMBMEBKhadv3VRfXK1ea3nLFrYfq38ltlSlRQLr_nWf0fkvDn-FpgE6LPapA")
+        self.upload = vk_api.VkUpload(self.vk_sess)
         self.album_id = ALBUM_ID
         self.group_id = GROUP_ID
-        try:
-            self.vk_sess.auth(token_only=True)
-        except vk_api.AuthError as e:
-            logging.error(f"Ошибка авторизации: {str(e)}")
-            print(f"Ошибка авторизации: {str(e)}")
-            return
 
-    @staticmethod
-    def two_factor_handler():
-        code = input("Введите код двухфакторной аутентификации: ")
-        return code, False
-
-    @staticmethod
-    def captcha_handler(captcha):
-        print(f"Откройте ссылку в браузере: {captcha.get_url()}")
-        return input("Введите текст капчи: ")
-
-    def run(self):
-        self.upload = vk_api.VkUpload(self.vk_sess)
+    def run(self, username, profile_link):
         for file_name in os.listdir(self.path):
             file_path = os.path.join(self.path, file_name)
             try:
+                caption = ""
+                if username and profile_link:
+                    caption = f"Автор: {username}\nПрофиль: {profile_link}"
                 self.upload.photo(
                     file_path,
                     album_id=self.album_id,
-                    group_id=self.group_id
+                    group_id=self.group_id,
+                    caption=caption
                 )
                 logging.info(f"График {file_name} успешно загружен!")
+                print(f"График {file_name} успешно загружен!")
             except Exception as e:
                 logging.error(f"Ошибка загрузки {file_name}: {str(e)}")
                 print(f"Ошибка загрузки {file_name}: {str(e)}")
